1ff9f1f509bc7bb3016ba6980fe16a47db621288
Calle <Statencb@gmail.com>
2022-05-23 14:00:37 +0200

    Fixade lite bilder mm. till countern

--- Assets/Scripts/ScoreCounter.cs
+++ Assets/Scripts/ScoreCounter.cs
@@ -7,9 +7,10 @@ public class ScoreCounter : MonoBehaviour
 {
 
     public Text counterText;
-    public Text multiplierText;
+    public Sprite[] multiplierImages;
+    public Image multiplierImage;
     public double score;
-    public double mulitplier;
+    public double multiplier;
     public int intScore;
 
     // Start is called before the first frame update
@@ -23,24 +24,30 @@ public class ScoreCounter : MonoBehaviour
     {
         intScore = Mathf.RoundToInt((float)score);
         counterText.text = intScore.ToString();
-        multiplierText.text = "x" + mulitplier.ToString();
-        if (GameController.shipsAlive == 3)
-        {
-            mulitplier = 1;
+        //multiplierText.text = mulitplier.ToString();
+        if (GameController.shipsAlive != 0) { 
+            if (GameController.shipsAlive == 3)
+            {
+                multiplier = 1;
+                multiplierImage.sprite = multiplierImages[0];
             
-        }
-        else if(GameController.shipsAlive == 2)
-        {
-            mulitplier = .8;
-            
-        }
-        else if (GameController.shipsAlive == 1)
-        {
-            mulitplier = .6;
-            
-        }
+            }
+            else if(GameController.shipsAlive == 2)
+            {
+                multiplier = .8;
+                multiplierImage.sprite = multiplierImages[1];
 
-        score += mulitplier;
+            }
+            else if (GameController.shipsAlive == 1)
+            {
+                multiplier = .6;
+                multiplierImage.sprite = multiplierImages[2];
+
+            }
+            score += multiplier;
+            Debug.Log(multiplier);
+        }
+        
 
         
     }
